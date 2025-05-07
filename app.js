36168870bb4202c2c3857bf5da70a1136ed52641
Harris Ryder <ryderharris@icloud.com>
2025-05-07 14:29:17 +0100

    mega push 🚀

--- app.js
+++ app.js
@@ -9,12 +9,16 @@ var https = require("https");
 
 var indexRouter = require("./routes/index");
 
+// Initialize Express app
+var app = express();
+
 // Set up rate limiter: maximum of twenty requests per minute
 const RateLimit = require("express-rate-limit");
 const limiter = RateLimit({
   windowMs: 1 * 60 * 1000, // 1 minute
   max: 5,
 });
+
 // Apply rate limiter to all requests
 app.use(limiter);
 
